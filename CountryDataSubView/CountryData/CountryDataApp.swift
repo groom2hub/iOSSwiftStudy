5fc5d4e08935d934ed52bd32661aafdc5f7457a8
groom2hub <groomwnsgur35@gmail.com>
2023-07-14 16:52:07 +0900

    Project: CountryData With Subview

---
+++ CountryDataSubView/CountryData/CountryDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountryDataApp.swift
+//  CountryData
+//
+//  Created by 최준혁 on 2023/07/11.
+//
+
+import SwiftUI
+
+@main
+struct CountryDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CountryDataSubView/CountryData/CountryDetailView.swift
@@ -0,0 +1,33 @@
+//
+//  CountryDetailView.swift
+//  CountryData
+//
+//  Created by 최준혁 on 2023/07/11.
+//
+
+import SwiftUI
+
+struct CountryDetailView: View {
+    let country: Country
+    var body: some View {
+        List {
+            Image(country.file)
+                .resizable()
+                .frame(width: 200, height: 200)
+            Text(country.name)
+            Text("\(country.name.count) million people")
+            Text("Blah Blah")
+            Text("Something")
+        }
+        .navigationTitle(country.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct CountryDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            CountryDetailView(country: Country(name: "대한민국🇰🇷", file: "south_korea"))
+        }
+    }
+}
