f7c7933962329d9da70db1c572d305be08d6276d
groom2hub <groomwnsgur35@gmail.com>
2023-07-14 16:51:09 +0900

    Project: CountryData

---
+++ CountryData/CountryData/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  CountryData
+//
+//  Created by 최준혁 on 2023/07/11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        List {
+            ForEach(Region.all, id: \.title) { region in
+                Section(region.title) {
+                    ForEach(region.countries, id: \.name) {
+                        country in
+                        HStack {
+                            Image(country.file)
+                            VStack{
+                                Text(country.name)
+                                    .font(.title)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                                Text("\(country.name.count) million people")
+                                    .foregroundColor(.gray)
+                                    .frame(maxWidth: .infinity, alignment: .leading)
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
