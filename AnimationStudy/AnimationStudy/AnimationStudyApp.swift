2da7313e4daae15d4ba2daf29b02b4b6b90449a0
groom2hub <groomwnsgur35@gmail.com>
2023-07-15 17:17:27 +0900

    Project: AnimationStudy

---
+++ AnimationStudy/AnimationStudy/AnimationStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationStudyApp.swift
+//  AnimationStudy
+//
+//  Created by 최준혁 on 2023/07/12.
+//
+
+import SwiftUI
+
+@main
+struct AnimationStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
