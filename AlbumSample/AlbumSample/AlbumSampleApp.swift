ebd46996c8152a311b3ffc2e078fc48328a55089
groom2hub <groomwnsgur35@gmail.com>
2023-07-17 23:15:19 +0900

    Project: AlbumSample

---
+++ AlbumSample/AlbumSample/AlbumSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlbumSampleApp.swift
+//  AlbumSample
+//
+//  Created by 최준혁 on 2023/07/15.
+//
+
+import SwiftUI
+
+@main
+struct AlbumSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AlbumListView()
+        }
+    }
+}
